edd865ed35741de03bfe377c79521643bdd4e319
Arun-s21 <arunsng18@gmail.com>
2025-08-26 15:34:05 +0530

    Added comments to the unique-username check api

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -3,14 +3,14 @@ import UserModel from '@/models/User';
 
 export async function POST(request:Request){
     await dbConnect();
-    try{
+    try{        //get username from the frontend
                 const {username} = await request.json();
-
+                //check if the username already exists in the db and if it is verified or not
                 const existingVerifiedUser = await UserModel.findOne({
                     username,
                     isVerified: true,
                 });
-
+                //if username exists return the message
                 if(existingVerifiedUser){
                     return Response.json(
                         { success: false, message: 'Username is already taken' },
